2bbdac729bd996413133eb42b7d5164a51b918e0
Anshuman <anshuman@Anshumans-MacBook-Pro.local>
2023-09-07 15:54:15 +0530

    project init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Richpanel Asssignment FB</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
